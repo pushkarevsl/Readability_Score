5fef4cfb66b1888796de100efacd4a0439a1d218
pushkarevsl <pqi2gb@mail.ru>
2020-06-04 22:59:46 +0300

    задачки

--- Problems/Compression algorithm/src/Main.java
+++ Problems/Compression algorithm/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner sc = new Scanner (System.in);
+        String num = sc.next ( );
+
+        char[] chars = num.toCharArray ( );
+        int counter = 1;
+        int i;
+
+        for (i = 1; i < chars.length; i++) {
+
+            if (chars[i - 1] == chars[i] && i <= chars.length - 1) {
+                counter++;
+                continue;
+            }
+            System.out.print (String.valueOf (chars[i - 1]) + counter);
+            counter = 1;
+        }
+        System.out.print (String.valueOf (chars[i - 1]) + counter);
     }
 }
