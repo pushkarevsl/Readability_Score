fa4d461d1db5419a5ae646df0572ebab5f51f010
pushkarevsl <pqi2gb@mail.ru>
2020-07-08 08:01:27 +0300

    задачки

--- Problems/Parse url/src/Main.java
+++ Problems/Parse url/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+        String parameters = line.split("\\?")[1];
+
+        for (String items : parameters.split("&")) {
+            if (items.split("=").length == 2) {
+                System.out.println(items.replaceAll("=", " : "));
+            } else {
+                System.out.println(items.replaceAll("=", " : ") + "not found");
+            }
+        }
+
+        if (parameters.contains("pass")) {
+            for (String items : parameters.split("&")) {
+                String[] temp = items.split("=");
+                if (temp[0].equals("pass")) {
+                    System.out.println("password : " + temp[1]);
+                }
+            }
+        }
     }
 }
