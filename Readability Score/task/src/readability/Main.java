2b18cb4370e90ebdb91ea4484ede728ac0dc28c6
pushkarevsl <pqi2gb@mail.ru>
2020-07-15 08:15:06 +0300

    regex stage 2/4

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -3,7 +3,24 @@ package readability;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] sentences = input.split("[.?!]");
+        String[] words = input.split("\\s");
+
+        double averageWords = (double) words.length / sentences.length;
+
+        if (averageWords > 10) {
+            System.out.println("HARD");
+        } else {
+            System.out.println("EASY");
+        }
+    }
+
+//    public static void main(String[] args) {
 //        String str1 = "aaabbcccdaa";
 //        String str2 = " ";
 //
@@ -14,5 +31,6 @@ public class Main {
 //            }
 //        }
 //        System.out.println (str2);
-    }
+//    }
 }
+
